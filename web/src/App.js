86917204c849461cc5774d5042824325438bdfb8
Leo Nadeak <leo@pomona.id>
2020-09-27 16:52:42 +0700

    feat(web): Implement CRUD task
    Create task, deket task, finish task, list task

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState, useCallback } from "react";
+import { v4 as Uuid } from "uuid";
+import "./App.css";
+
+import TaskStore from "store/tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [updatedTaskID, setUpdatedTaskID] = useState([]);
+  const [form, setForm] = useState({});
+
+  const onChangeForm = useCallback(
+    (name, value) => {
+      setForm({
+        ...form,
+        [name]: value,
+      });
+    },
+    [form]
+  );
+
+  useMemo(async () => {
+    if (!TaskStore.isInitialized) {
+      await TaskStore.initialize();
+    }
+
+    setTasks(TaskStore.data);
+  }, []);
+
+  const deleteTask = useCallback(
+    (id) => async () => {
+      const task = tasks[id];
+      await TaskStore.deleteItem(task._id);
+      const newTask = [...tasks];
+      newTask.splice(id, 1);
+      setTasks(newTask);
+    },
+    [tasks]
+  );
+
+  const updateTask = useCallback(
+    (id) => () => {
+      setUpdatedTaskID(id);
+    },
+    []
+  );
+
+  const finishTask = useCallback(
+    (id) => async () => {
+      const task = tasks[id];
+      await TaskStore.editItem(task._id, {
+        status: true,
+      });
+
+      const newTask = [...tasks];
+      newTask[id].status = true;
+
+      setTasks(newTask);
+    },
+    [tasks]
+  );
+
+  const onSave = useCallback(async () => {
+    const id = Uuid();
+    await TaskStore.addItemWithId(id, {
+      ...form,
+      tags: form.tags.split(","),
+      created_at: new Date(),
+      status: false,
+    });
+  }, [form]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={onSave}>
+        <input
+          name="content"
+          onChange={({ target: { name, value } }) => {
+            onChangeForm(name, value);
+          }}
+        />
+        <input
+          name="tags"
+          onChange={({ target: { name, value } }) => {
+            onChangeForm(name, value);
+          }}
+        />
+        <button type="submit">Simpan</button>
+      </form>
+      {tasks.map(
+        (task, key) =>
+          !!task._id && (
+            <div key={key} className="task-item">
+              <span className="task-content">{task.content}</span>
+              <span className="task-tag">
+                {task.tags ? task.tags.join(", ") : ""}
+              </span>
+              <span className="task-created">{task.created_at}</span>
+              <span className="task-status">
+                {task.status ? "Done" : "In Progress"}
+              </span>
+              <button className="task-delete" onClick={deleteTask(key)}>
+                Delete
+              </button>
+              <button className="task-finish" onClick={finishTask(key)}>
+                Finish Task
+              </button>
+              <button classname="task-update" onClick={updateTask(key)}>
+                Update
+              </button>
+            </div>
+          )
+      )}
     </div>
   );
 }
---
+++ web/src/config/index.js
@@ -0,0 +1,5 @@
+export default {
+  POUCH_URL: "http://13.250.43.79:5984/",
+  POUCH_USERNAME: "admin",
+  POUCH_PASSWORD: "iniadmin",
+};
